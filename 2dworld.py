5c1471aa027c222283544f5067e67d45f05056fe
makingmusic <harinder@gmail.com>
2025-05-17 21:50:58 -0400

    back to 10

--- 2dworld.py
+++ 2dworld.py
@@ -24,7 +24,7 @@ epsilon_min = 0.01
 
 # Grid Configuration Variables 
 num_episodes = 1000  # number of training episodes
-grid_size = 40  # size of the 2D grid (grid_size x grid_size)
+grid_size = 10  # size of the 2D grid (grid_size x grid_size)
 start_pos = (0, 0)  # starting position
 goal_pos = (grid_size-1, grid_size-1)  # goal position
 
